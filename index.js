34ada3ca123e9170e01aa9c25c8a88d164b6cd4c
Mehedi Hasan <mehedi609@gmail.com>
2022-01-06 17:09:10 +0600

    settings some globally used thing and font-size

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const params = {
   file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   // wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
   // mount: [['/components', './node_modules']], // Mount a directory to a route.
-  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots,
+  watch: "css,img,js",
 };
 
 liveServer.start(params);
